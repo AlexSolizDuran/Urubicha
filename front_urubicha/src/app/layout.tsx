090b476ca561ac357933cb2e48976938376112c5
ByCrilux-XMode <christian.estudia.sistemas@gmail.com>
2025-07-30 19:38:36 -0400

    NavBar prototipo1

--- front_urubicha/src/app/layout.tsx
+++ front_urubicha/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import NavBar from "@/components/Navbar/NavBar";
 import "@/styles/globals.css";
-import Navbar from "@/components/Navbar/Navbar";
 
 export default function RootLayout({
   children,
@@ -10,9 +10,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+      <title> uruicha </title>
+      </head>
       <body>
-        <Navbar></Navbar>
-        <div className="p-4">{children}</div>
+        <header> <NavBar/></header>
+        <main className="p-4">{children}</main>
+        <footer>{/*componente de pie de pagina*/}</footer>
       </body>
     </html>
   );
