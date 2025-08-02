dbd0bd285db3803f0d27e2e74e931e9be430da5a
AlexSolizDuran <solizelex1999@gmail.com>
2025-08-02 18:08:26 -0400

    estructurando carpetas

--- front_urubicha/src/app/(publico)/tienda/page.tsx
+++ front_urubicha/src/app/(publico)/tienda/page.tsx
@@ -1,3 +1,3 @@
 export default function page(){
-    return <div> esta pestaña tiene informacion sobre los productos</div>
+    return <div> esta pestaña tiene un resumen sobre los productos</div>
 }
